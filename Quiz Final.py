a17c628e53f33d950e72514ce9ed171e97e7c5d6
Windy4 <wackywindy4@gmail.com>
2023-05-25 19:48:44 +1200

    fixed int checker

--- Quiz Final.py
+++ Quiz Final.py
@@ -1,23 +1,25 @@
-#Declare the variables for the second quiz
+# Declare the variables for the second quiz
 questions_2 = ["What is the largest planet in our solar system?", "Which animal is known for its black and white stripes?"]
 answers_2 = [["Earth", "Mars", "Jupiter", "Saturn"], ["Elephant", "Tiger", "Zebra", "Giraffe"]]
 correct_2 = [3, 3]
-#Declare the variables for the first quiz
-questions_1 = ["What is the captial of france?", "What is the formula for pressure?", "Who is credited with inventing the World Wide Web?"]
+# Declare the variables for the first quiz
+questions_1 = ["What is the capital of france?", "What is the formula for pressure?", "Who is credited with inventing the World Wide Web?"]
 answers_1 = [["Paris", "Italy", "Germany", "London"], ["p=f*a", "a=p/f", "p=f/a", "P=a/f"], ["Tim Berners-Lee", "Bill Gates", "Steve Jobs", "Mark Zuckerberg"]]
 correct_1 = [1, 3, 1]
-#Declare the variable for the differance between quizes
-minage = 10
+# Declare the variable for the differance between quiz's
+min_age = 10
+
 
 def print_question(question):
-    #Prints the required question
-    #used to make code more efficient
+    # Prints the required question
+    # used to make code more efficient
     print("----------------------------")
     print(question)
     print("----------------------------")
 
+
 def print_answers(answer, age):
-    if age <= int(minage):
+    if age <= int(min_age):
         index = questions_2.index(answer)
         print("Answers")
         print("----------------------------")
@@ -31,7 +33,6 @@ def print_answers(answer, age):
         for answer in answers_1[index]:
             print(str(answer))
         print("----------------------------")
-
 
 
 def start_young_quiz(age):
@@ -49,9 +50,10 @@ def start_young_quiz(age):
     quiz_questions = len(questions_2)
     print("your score is {}".format(score_2) + " out of ", quiz_questions)
 
-def start_quiz (age):
+
+def start_quiz(age):
     """
-    Had problem with score, used gpt to tell me to move the score outside of for loop,
+    Had problem with score, used gpt to tell me to move the score outside for loop,
     this is the answer gpt gave me
 
     "I see that you're having an issue with the score count section.
@@ -78,16 +80,17 @@ def start_quiz (age):
     print("your score is {}".format(score) + " out of ", quiz_questions)
 
 
-class age_verification ():
-    check = 1
-    while check == 1:
+class Age_verification:
+    while True:
         age = input("How old are you?: ")
-        if isinstance(age, int):
-            check -= 0
-            if int(age) <= int(minage):
+        try:
+            age = int(age)  # Convert the input to an integer
+            if age <= min_age:
                 start_young_quiz(age)
             else:
                 start_quiz(age)
-        else:
-            print("Non Integer, try again")
+            break  # Exit the loop if everything is successful
+        except ValueError:
+            print("Non-integer input, try again")
+
 
